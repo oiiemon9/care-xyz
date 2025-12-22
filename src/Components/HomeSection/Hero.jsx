b2af857cbf388cbe1db34d66c373af87fb47708e
oiiemon9 <oiiemon9@gmail.com>
2025-12-22 15:03:47 +0600

    hero section edit

--- src/Components/HomeSection/Hero.jsx
+++ src/Components/HomeSection/Hero.jsx
@@ -10,10 +10,11 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Autoplay, EffectFade, Pagination } from 'swiper/modules';
 import Image from 'next/image';
+import { ChevronRight } from 'lucide-react';
 
 export default function Hero() {
   return (
-    <div>
+    <div className="relative">
       <Swiper
         effect={'fade'}
         pagination={{
@@ -64,6 +65,24 @@ export default function Hero() {
           />
         </SwiperSlide>
       </Swiper>
+      <div className=" absolute top-0 z-10 flex items-center h-full w-full ">
+        <div className="container mx-auto px-4 flex items-center">
+          <div className="max-w-2xl bg-neutral/50  border-white/20 rounded-2xl shadow-2xl p-8 md:p-12 text-base-200">
+            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold  leading-tight mb-6">
+              Trusted Care for Your Loved Ones, Anytime
+            </h1>
+            <p className="text-md md:text-xl mb-8 max-w-xl leading-relaxed">
+              Professional, reliable, and compassionate caregiving services for
+              children, elderly, and those in need. Safe, easy, and always
+              accessible.
+            </p>
+            <button className="group inline-flex items-center gap-2 bg-primary  px-8 py-4 rounded-full text-lg font-medium hover:bg-primary/90 transition-all duration-300 shadow-lg hover:shadow-xl">
+              Book Now
+              <ChevronRight className="w-5 h-5 transition-transform group-hover:translate-x-1" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
