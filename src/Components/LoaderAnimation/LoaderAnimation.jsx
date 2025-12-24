b5c5fbd151c864f7fb657bbf69ae7435fd7bc6d9
oiiemon9 <oiiemon9@gmail.com>
2025-12-24 12:28:06 +0600

    loading edit

--- src/Components/LoaderAnimation/LoaderAnimation.jsx
+++ src/Components/LoaderAnimation/LoaderAnimation.jsx
@@ -2,14 +2,13 @@
 import React from 'react';
 
 import Lottie from 'lottie-react';
-import loadingAnimation from '/loadingAnimation.json';
 
 export default function LoaderAnimation() {
   return (
     <div className="flex justify-center items-center py-10">
       <Lottie
         className="h-20 w-20"
-        animationData={loadingAnimation}
+        part="/loadingAnimation.json"
         loop={true}
         autoplay={true}
       />
